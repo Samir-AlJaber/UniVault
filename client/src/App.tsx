8ddf4f56f3eb83507bdb793e53bebac5e5d22456
TarinRifa <tarinrifa@gmail.com>
2026-04-04 21:41:14 +0600

    routing path added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-
+import Resources from "./pages/Resources";
+import UploadResources from "./pages/UploadResources";
+import DownloadResources from "./pages/DownloadResources";
+import Profile from "./pages/Profile";
 
 function App() {
 
@@ -19,7 +22,14 @@ function App() {
 
         <Route path="/register" element={<Register />} />
 
-        
+        <Route path="/resources" element={<Resources />} />
+
+        <Route path="/upload" element={<UploadResources />} />
+
+        <Route path="/download" element={<DownloadResources />} />
+
+        <Route path="/profile" element={<Profile />} />
+
       </Routes>
 
     </BrowserRouter>
