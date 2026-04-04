c460cadf19da92b90d3661136a52d3201c763e78
Raiyan <fahim.cse.20220204098@aust.edu>
2026-04-04 21:23:43 +0600

    Download function added

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SemesterController;
 use App\Http\Controllers\ResourceController;
@@ -10,21 +9,9 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/semesters', [SemesterController::class, 'index']);
-
-
 Route::get('/courses/{semester}', [ResourceController::class, 'getCourses']);
-
-
 Route::get('/resources/{course}', [ResourceController::class, 'getResources']);
 
-
 Route::post('/upload', [ResourceController::class, 'upload']);
-
-
 Route::delete('/delete/{id}', [ResourceController::class, 'delete']);
-
-
 Route::get('/my-resources/{user_id}', [ResourceController::class, 'getUserResources']);
-
-
-Route::get('/download/{id}', [ResourceController::class, 'download']);
